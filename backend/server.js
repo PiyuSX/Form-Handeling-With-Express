8ef29c4fdc4d99b320ccb03abc99cf611a6b1fd2
Piyush <piyman905@gmail.com>
2025-07-10 12:26:26 +0545

    Backend Upadte

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(bodyParser.json())
+
+let submittedData = null  // store the form data
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    if (submittedData) {
+        res.send(`
+            <h1>Form Submitted</h1>
+            <p><strong>Username:</strong> ${submittedData.username}</p>
+            <p><strong>Password:</strong> ${submittedData.password}</p>
+        `)
+    } else {
+        res.send('Hello World!')
+    }
 })
 
 app.post('/', (req, res) => {
+    submittedData = req.body
     console.log(req.body)
-    res.send('Form submitted!')
+    res.redirect('/')  
 })
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
